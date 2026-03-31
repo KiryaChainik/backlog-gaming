b609f1fc94d9d0dd38ef7214831a86ca73e6821d
chainik <nefekirik@gmail.com>
2026-03-31 20:43:54 +0300

    Initial Commit

---
+++ Backlog_Gaming/Backlog_GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Backlog_GamingApp.swift
+//  Backlog_Gaming
+//
+//  Created by Кирилл Пантюхов on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Backlog_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
